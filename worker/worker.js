3f611b24cf9bf897b6fd5f9038e6c2679de88dca
Hajime Hoshi <hajimehoshi@gmail.com>
2021-12-08 15:21:50 +0900

    fix

--- worker/worker.js
+++ worker/worker.js
@@ -1,8 +1,10 @@
+let i = 0;
 let now = Date.now();
 for (;;) {
   const newNow = Date.now();
   if (newNow - now >= 1000) {
-    postMessage(newNow);
+    postMessage(i);
+    i++;
     now = newNow;
   }
 }
